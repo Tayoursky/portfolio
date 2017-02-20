554a73dc2c2275a050a2f95285d58931394da3b9
Boris Tayoursky <tayoursky@gmail.com>
2017-02-20 15:14:55 +0900

    mail form

--- mail.php
+++ mail.php
@@ -3,22 +3,14 @@
 $recepient = "youmail@test.loc";
 $sitename = "www.yousite.test";
 
-$name = trim($_GET["name"]);
-$email = trim($_GET["email"]);
-$text = trim($_GET["text"]);
 
-$name = htmlspecialchars($name);
-$email = htmlspecialchars($email);
-$text = htmlspecialchars($text);
+$name = substr(htmlspecialchars(trim($_POST["name"])), 0, 100);
+$email = substr(htmlspecialchars(trim($_POST["email"])), 0, 100);
+$text = substr(htmlspecialchars(trim($_POST["text"])), 0, 100000); 
 
-$name = urldecode($name);
-$email = urldecode($email);
-$text = urldecode($text);
-
-$name = trim($name);
-$email = trim($email);
-$text = trim($text);
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
 $message = "Имя: $name \nE-mail: $email \nСообщение: $text";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+
+?>
